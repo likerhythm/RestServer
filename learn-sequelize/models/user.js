460e585e607f209edb11ba1f0f07cac335e32e38
likerhythm <89333351+likerhythm@users.noreply.github.com>
2022-06-04 23:31:18 +0900

    주석 추가

--- learn-sequelize/models/user.js
+++ learn-sequelize/models/user.js
@@ -27,10 +27,15 @@ module.exports = class User extends Sequelize.Model {
       },
     }, {
       sequelize,
+		// timestamps: true이면 createdAt과 updatedAt을 자동으로 생성해줌
       timestamps: false,
+		// createdAt -> created_at으로 자동 변경해줌
       underscored: false,
+		// 모델이름은 js에서 쓰는 이름, 테이블이름은 DB에서 쓰는 이름.
+		// 모델 이름이 User 이면 DB에서의 테이블 이름은 users이다.
       modelName: 'User',
       tableName: 'users',
+		// paranoid: true이면 deletedAt 기능 사용 가능
       paranoid: false,
       charset: 'utf8',
       collate: 'utf8_general_ci',
