00ef571d0e6a4bd5c3810ec87b7ba81bccf1c50d
likerhythm <89333351+likerhythm@users.noreply.github.com>
2022-06-04 15:02:34 +0900

    시퀄라이즈 실습 따라하기

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -6,6 +6,9 @@ const nunjucks = require('nunjucks');
 // require('./models')는 require('./models/index.js')와 같음
 // 시퀄라이즈 연결 객체(db.sequelize) 가져오기
 const { sequelize } = require('./models');
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
 
 const app = express();
 // 포트 설정
@@ -32,6 +35,10 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
